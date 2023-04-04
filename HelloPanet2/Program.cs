8dc07510d2ee016d71c64a0479976711c48c7405
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-04 13:07:00 -0400

    Variables and Constants

--- HelloPanet2/Program.cs
+++ HelloPanet2/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+int number = 2;
+int numerical = 3;
+const float Pi = 3.14f;
+int otherNumber = number * numerical;
+
+Console.WriteLine(otherNumber);
