28f2ea977f607742da60877f2a410d46df98198c
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-04 13:32:32 -0400

    Demo of Variables and Constants

--- HelloPanet2/Program.cs
+++ HelloPanet2/Program.cs
@@ -1,9 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 3;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Sandy";
+            Console.WriteLine(firstName);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(count);
 
-int number = 2;
-int numerical = 3;
-const float Pi = 3.14f;
-int otherNumber = number * numerical;
-
-Console.WriteLine(otherNumber);
+        }
+    }
+}
